47126fe17b016d0c39af1faf4ecdb4122aaf7c97
ZAS-XD <136427364+ZAS-XD@users.noreply.github.com>
2026-03-27 02:21:07 +0800

    Delete TOLLS directory

--- TOLLS/launch_fnv_skip_launcher.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Meluncurkan Fallout New Vegas tanpa launcher (skip loop)...
-cd /d "C:\Program Files (x86)\Steam\steamapps\common\Fallout New Vegas"
-start steam://rungameid/22380 -skipLauncher -windowed
-echo Jika gagal, coba run sebagai admin dan pilih "Run anyway" jika ada popup.
-pause
--- TOLLS/launch_fnv_win11_fix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Meluncurkan Fallout New Vegas dengan fix Win11 launcher loop + compat mode...
-cd /d "C:\Program Files (x86)\Steam\steamapps\common\Fallout New Vegas"
-PowerShell -Command "Start-Process 'falloutnv.exe' -ArgumentList '-skipLauncher','-dx9single','-windowed' -Verb RunAs -WorkingDirectory $PWD"
-echo Jalankan sebagai admin manual jika PowerShell block.
-echo Coba ini jika yang pertama gagal. Right-click bat file ini dan "Run as administrator".
-pause
